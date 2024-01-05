b752b03bae577cc8012e0722a5d5e88f2ec0930d
ThangNguyen157 <86416687+ThangNguyen157@users.noreply.github.com>
2024-01-04 23:27:49 -0500

    add ability to choose the text to keep track on

--- _init_.py
+++ _init_.py
@@ -1,6 +1,8 @@
-from bs4 import BeautifulSoup as bs
+import bs4
 import requests
-def get_input():
+from lxml import etree
+text = []
+def getInput():
     url = input("Enter url of the page: ");
     while True:
         try:
@@ -13,21 +15,52 @@ def get_input():
             print("Invalid input. Try again.")
     return url, time_interval
 
-
-
-def main():
-    url, time_interval = get_input()
+def scraping(url):
     splash_url = 'http://localhost:8050/render.html'
 
     params = {
     'url': url,
-    'wait': 5,  # Time to wait for JavaScript to execute (adjust as needed)
+    'wait': 2,  # Time to wait for JavaScript to execute (adjust as needed)
     'render_all': 1,
     }
     data = requests.get(splash_url, params = params)
 
-    soup = bs(data.text, "html.parser")
-    print(soup.prettify())
+    soup = bs4.BeautifulSoup(data.text, "html.parser")
+    return soup
+
+def getinput2(length):
+    while True:
+        try:
+            number = int(input("Enter the number associates the text you want to check: "))
+            if number < length and number > 0:
+                break
+            else:
+                print("The number should be between range 0-"+str(length)+".")
+        except ValueError:
+            print("Invalid input. Try again")
+    return number
+
+
+def main():
+    url, timeInterval = getInput()
+    html = scraping(url)
+
+    text = []
+    indeces = []
+    content = list(html.descendants)
+
+    for index, value in enumerate(content):
+        if type(value) == bs4.element.NavigableString and not value.isspace():
+            while '\n' in value:
+                value = value.replace('\n', "")
+            text.append(value)
+            indeces.append(index)
+    for index, value in enumerate(text):
+        print(str(index+1) + ". " + value)
+
+    number = getinput2(len(text))
+    print(content[indeces[number-1]-1].parent)
+    
 
 if __name__ == "__main__":
     main()

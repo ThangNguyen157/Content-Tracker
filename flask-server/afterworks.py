06c5806dc05c78c7228090af4d8ff37ab2df8575
ThangNguyen157 <86416687+ThangNguyen157@users.noreply.github.com>
2024-01-15 18:19:05 -0500

    update checkContent function the funciton now can check for changes in attributes, tag name, and text

--- flask-server/afterworks.py
+++ flask-server/afterworks.py
@@ -16,24 +16,75 @@ def getSleepTime(conn, cur):
     diff = dataDB[4] - notTimeZoneAware
     return diff.total_seconds(), dataDB
 
-def checkContent(cur, dataDB):
+def checkContent(dataDB):
     newValue = []
     oldValue = []
     changeType = []
+    newTag = []
+    oldTag = []
+    #if can not scrape
     data = Scrape(dataDB[0])
     content = data.scrape()
     soup = data.getModifiedHTML(content)
+
+    #find change in text
     for index, id in enumerate(data[5]):
         minisoup = BeautifulSoup(data[6][index], 'html.parser')
+        minisoup.find(id=id).unwrap()
         tag = soup.find(id=id)
-        if (tag.get_text() != minisoup.get_text()):
-            oldValue.append(tag)
-            newValue.append(minisoup)
-    return newValue, oldValue
+        parent = tag.parent
+        parent.span.unwrap()
+        if len(newValue < index+1):
+                oldTag.append([])
+                newTag.append([])
+                oldValue.append([])
+                newValue.append([])
+                changeType.append([])
+        if parent.get_text() != minisoup.get_text():
+            oldTag[index].append(str(minisoup.prettify()))
+            newTag[index].append(str(parent.prettify()))
+            oldValue[index].append(str(minisoup.get_text()))
+            newValue[index].append(str(parent.get_text()))
+            changeType[index].append('Change in content')
+        if parent.name != minisoup.name:
+            oldTag[index].append(str(minisoup.prettify()))
+            newTag[index].append(str(parent.prettify()))
+            oldValue[index].append(str(minisoup.name))
+            newValue[index].append(str(parent.name))
+            changeType[index].append('Change in tag name')
+        
+        if len(parent.attrs) > len(minisoup.attrs):
+            big = parent.attrs
+            small = minisoup.attrs
+        else:
+            big = minisoup.atrrs
+            small = parent.attrs
+        for key in (list(big.keys())):
+            if key not in list(small.keys()):
+                oldTag[index].append(str(minisoup.prettify()))
+                newTag[index].append(str(parent.prettify()))
+                oldValue[index].append('not having that attribute before or it was removed')
+                newValue[index].append(key+ ' = '+ big['key'])
+                changeType[index].append('new attribute added/removed')
+            elif minisoup.attrs[key] != parent.attrs[key]:
+                oldTag[index].append(str(minisoup.prettify()))
+                newTag[index].append(str(parent.prettify()))
+                oldValue[index].append(minisoup.attrs[key])
+                newValue[index].append(parent.attrs[key])
+                changeType[index].append("Change in attribute's value")
+        if not newTag[-1]:
+            oldTag.pop()
+            newTag.pop()
+            oldValue.pop()
+            newValue.pop()
+            changeType.pop()
 
-def updateDB(conn, cur, dataDB, newValue=[]):
+    return newTag, oldTag, newValue, oldValue, changeType
+
+def updateDB(conn, cur, dataDB, newTag=[]):
     #list is mutable so need to make a copy to avoid changing the original list
-    nv = copy.copy(newValue)
+    nv = copy.copy(newTag)
+
     notTimeZoneAware = datetime.datetime.now(tz=datetime.UTC)
     newNext_run = notTimeZoneAware + timedelta(minutes=dataDB[2])
     for index, i in enumerate(dataDB[5]):
@@ -56,16 +107,14 @@ def main():
         seconds, dataDB = getSleepTime(conn, cur)
         if (seconds > 4):
             break
-        oldValue, newValue = checkContent(cur, dataDB)
+        oldTag, newTag, newValue, oldValue, changeType = checkContent(dataDB)
 
-        if newValue:
-            updateDB(conn, cur, dataDB, newValue)
+        if newTag:
+            updateDB(conn, cur, dataDB, newTag)
             #send email,....
             #check reply email,....
         else:
             updateDB(conn, cur, dataDB)
-
-    
     
     conn.close()
     cur.close()
